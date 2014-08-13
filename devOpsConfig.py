8441c0fab2dc871a380bd04e3b24ab54a2cd4e07
DrewEasley <github@4Easleys.com>
2014-08-13 15:48:14 -0400

    Initial Commit of devOpsConfig.py

---
+++ devOpsConfig.py
@@ -0,0 +1,21 @@
+#devOps.config.py
+
+
+#The Test_Designator is used to determine if we are using a Test Environment
+#For example, if your Dashboard is deployed to C:\Inetpub\wwwroot
+#but the test environment is in C:\Inetpub\wwwroot_test, then the TEST_DESIGNATOR
+#should be "_test"
+
+
+
+
+globalConfig = {}
+
+globalConfig['TEST_DESIGNATOR'] = "_test"
+
+
+#Config settings specific to modWeb only
+modWebConfig = {}
+modWebConfig['CGIDEBUG_TEST'] = True
+modWebConfig['CGIDEBUG_PROD'] = False #Not yet supported, nice to have
+
